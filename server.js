a64fbed9b1fce1bfe2b78e8835f7ebb4bd076a58
abhishek <pabhishekm@gmail.com>
2023-09-18 19:41:17 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(cors());
+require('dotenv').config();
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tn0wwt0.mongodb.net/`)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const dataSchema = new mongoose.Schema({
+  item: String,
+  strike: Boolean,
+});
+
+const model = mongoose.model("todo", dataSchema);
+
+app.post("/items", (req, res) => {
+  const data = new model(req.body);
+  data
+    .save()
+    .then((response) => {
+      res.send(req.body);
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/get-items", (req, res) => {
+  model
+    .find()
+    .then((response) => {
+      console.log(response);
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/delete-data/:_id", (req, res) => {
+  model
+    .findByIdAndDelete(req.params._id)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/", (req, res) => {
+  console.log("Hello world");
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
