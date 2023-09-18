71854438565997f577062aff26046b59ac6a2d4f
abhishek <pabhishekm@gmail.com>
2023-09-18 22:02:44 +0530

    Minor updates

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.delete("/delete-data/:_id", (req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-app.listen(port,"0.0.0.0", () => {
+app.listen(port, () => {
   console.log("Listening on port 3000");
 });
